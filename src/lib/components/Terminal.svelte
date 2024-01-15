f0d8e0a28628f5f372f09c3eb8c98d4a5f260008
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-01-15 21:50:29 +0700

    static build

--- src/lib/components/Terminal.svelte
+++ src/lib/components/Terminal.svelte
@@ -76,26 +76,6 @@
 		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-
 	.terminal {
 		background-color: #777799bb;
 	}
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -18,9 +18,6 @@
 			<li aria-current={$page.url.pathname.startsWith('/blogs') ? 'page' : undefined}>
 				<a href="/about">Blogs</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/projects') ? 'page' : undefined}>
-				<a href="/sverdle">Projects</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
