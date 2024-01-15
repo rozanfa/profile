049ac0c9c87fc8114d696eb9774122dbde2626e3
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-01-15 21:54:34 +0700

    Updates

--- _app/immutable/chunks/paths.Qwc88npU.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_bx3ntn?.base??"/sveltekit-github-pages",t=globalThis.__sveltekit_bx3ntn?.assets??s;export{t as a,s as b};
---
+++ _app/immutable/chunks/paths.hyuJKT6B.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1dya0xf?.base??"/sveltekit-github-pages",a=globalThis.__sveltekit_1dya0xf?.assets??s;export{a,s as b};
